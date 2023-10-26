202a3db3582bf269d1abc20769a4bca155681316
Adetops <heritagediwamu@gmail.com>
2023-10-26 12:46:40 -0700

    nginx config

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,32 +1,15 @@
-# install and configure nginx
-exec { 'update':
-  command     => '/usr/bin/apt-get update',
-  onlyif      => '/usr/bin/which nginx',
-}
--> package { 'nginx':
-  ensure => installed,
-}
--> exec { 'run1':
-  command => '/usr/bin/mkdir -p "/data/web_static/releases/test/" "/data/web_static/shared/"',
-}
--> exec { 'run2':
-  command => '/usr/bin/echo "Hi!" | sudo tee /data/web_static/releases/test/index.html > /dev/null',
-}
--> exec { 'run3':
-  command => '/usr/bin/rm -rf /data/web_static/current',
-}
--> exec { 'run4':
-  command => '/usr/bin/ln -s /data/web_static/releases/test/ /data/web_static/current',
-}
--> exec { 'run5':
-  command => '/usr/bin/chown -R ubuntu:ubuntu /data/',
-}
--> file_line { 'location_hbnb_static':
-  path     => '/etc/nginx/sites-available/default',
-  match    => '^server {',
-  line     => "server {\n\tlocation /hbnb_static {alias /data/web_static/current/;index index.html;}",
-  multiple => false,
-}
--> exec { 'run6':
-  command => '/usr/sbin/service nginx restart',
-}
+# Redoing task 0: Installs nginx and configures server
+exec { '/usr/bin/env apt-get -y update' : }
+-> exec { '/usr/bin/env apt-get -y install nginx' : }
+-> exec { '/usr/bin/env sed -i "/listen \[::\]:80 default_server/ a\\\trewrite ^/redirect_me http://www.holbertonschool.com permanent;" /etc/nginx/sites-available/default' : }
+-> exec { '/usr/bin/env sed -i "/listen \[::\]:80 default_server/ a\\\tadd_header X-Served-By \"\$HOSTNAME\";" /etc/nginx/sites-available/default' : }
+-> exec { '/usr/bin/env sed -i "/redirect_me/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default' : }
+-> exec { '/usr/bin/env # echo "Ceci n\'est pas une page" > /var/www/html/custom_404.html' : }
+-> exec { '/usr/bin/env service nginx start' : }
+-> exec { '/usr/bin/env mkdir -p /data/web_static/releases/test/' : }
+-> exec { '/usr/bin/env mkdir -p /data/web_static/shared/' : }
+-> exec { '/usr/bin/env echo "Hello Holberton School!" > /data/web_static/releases/test/index.html' : }
+-> exec { '/usr/bin/env ln -sf /data/web_static/releases/test/ /data/web_static/current' : }
+-> exec { '/usr/bin/env sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default' : }
+-> exec { '/usr/bin/env service nginx restart' : }
+-> exec { '/usr/bin/env chown -R ubuntu:ubuntu /data/' : }
